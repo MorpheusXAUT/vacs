0e04f7f8f60dc945bba8dd40d34650f7722bbc6a
Luis Kreihsl <luis.kreihsl@gmx.at>
2025-06-26 13:21:44 +0200

    feat(vacs-client): add tauri setup

--- vacs-client/src/config.rs
+++
@@ -1,9 +0,0 @@
-use serde::Deserialize;
-use vacs_audio::config::AudioConfig;
-use vacs_webrtc::config::WebrtcConfig;
-
-#[derive(Debug, Deserialize)]
-pub struct AppConfig {
-    pub audio: AudioConfig,
-    pub webrtc: WebrtcConfig,
-}
