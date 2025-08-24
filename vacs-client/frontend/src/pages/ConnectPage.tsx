88c49c0b4d8fe648385597ce7fdf9b01b14de9e5
Nick Müller <nick.mueller@mailbox.org>
2025-08-24 19:18:17 +0200

    feat(vacs-client): add terminate overlay and loading state to connect page

--- vacs-client/frontend/src/pages/ConnectPage.tsx
+++ vacs-client/frontend/src/pages/ConnectPage.tsx
@@ -1,21 +1,61 @@
 import Button from "../components/ui/Button.tsx";
-import {useAsyncDebounce} from "../hooks/debounce-hook.ts";
-import {invokeSafe} from "../error.ts";
+import {useAsyncDebounceState} from "../hooks/debounce-hook.ts";
+import {invoke} from "@tauri-apps/api/core";
+import {invokeSafe, isError, openErrorOverlayFromUnknown} from "../error.ts";
+import {useState} from "preact/hooks";
+import {clsx} from "clsx";
 
 function ConnectPage() {
-    const handleConnectClick = useAsyncDebounce(async () => {
-        await invokeSafe("signaling_connect");
+    const [terminateDialogOpen, setTerminateDialogOpen] = useState<boolean>(false);
+
+    const [handleConnectClick, loading] = useAsyncDebounceState(async () => {
+        try {
+            await invoke("signaling_connect");
+        } catch (e) {
+            if (isError(e) && e.message === "login failed: DuplicateId") {
+                setTerminateDialogOpen(true);
+                return;
+            }
+            openErrorOverlayFromUnknown(e);
+        }
+    });
+
+    const [handleTerminateClick, terminateLoading] = useAsyncDebounceState(async () => {
+        await invokeSafe("signaling_terminate");
+        setTerminateDialogOpen(false);
+        void handleConnectClick();
     });
 
     return (
         <div className="h-full w-full flex justify-center items-center p-4">
             <Button
                 color="green"
-                className="w-auto px-10 py-3 text-xl"
+                className={clsx("w-44 px-5 py-3 text-xl", loading && "brightness-90 cursor-not-allowed")}
                 onClick={handleConnectClick}
             >
-                Connect
+                {!loading ? "Connect" : "Connecting..."}
             </Button>
+
+            <div
+                style={{display: terminateDialogOpen ? "flex" : "none"}}
+                className="z-50 absolute top-0 left-0 w-full h-full justify-center items-center bg-[rgba(0,0,0,0.5)]"
+            >
+                <div
+                    className="bg-gray-300 border-4 border-t-red-500 border-l-red-500 border-b-red-700 border-r-red-700 rounded w-100 py-2">
+                    <p className="w-full text-center text-lg font-semibold wrap-break-word">Already connected</p>
+                    <p className="w-full text-center wrap-break-word mb-2">
+                        Your CID is already connected to the server.
+                        Do you wish to terminate the other client and connect anyways?
+                    </p>
+                    <div
+                        className={clsx("w-full flex flex-row gap-2 justify-center items-center mb-2", terminateLoading && "brightness-70 [&>button]:cursor-not-allowed")}>
+                        <Button color="red" className="px-3 py-1"
+                                onClick={() => setTerminateDialogOpen(false)}>No</Button>
+                        <Button color="green" className="px-3 py-1" onClick={handleTerminateClick}>Yes</Button>
+                    </div>
+                    {terminateLoading && <p className="w-full text-center font-semibold">Terminating...</p>}
+                </div>
+            </div>
         </div>
     );
 }
