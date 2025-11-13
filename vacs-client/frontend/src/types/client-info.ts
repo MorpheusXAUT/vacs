f38a0313a2c8147800f447fd1ed7946b36180496
Nick Müller <nick.mueller@mailbox.org>
2025-11-13 12:25:29 +0100

    fix(vacs-client): sort client DA keys sorting by station type, then station name (purely alphabetical)

--- vacs-client/frontend/src/types/client-info.ts
+++ vacs-client/frontend/src/types/client-info.ts
@@ -13,3 +13,20 @@ export function splitDisplayName(displayName: string): [string, string] {
 
     return [parts.slice(0, parts.length - 1).join(" "), parts[parts.length - 1]];
 }
+
+export function sortClients(clients: ClientInfo[]): ClientInfo[] {
+    return clients.sort((a, b) => {
+        const [aStationName, aStationType] = splitDisplayName(a.displayName);
+        const [bStationName, bStationType] = splitDisplayName(b.displayName);
+
+        // Sort clients with no station types last
+        if (aStationType.length === 0 && bStationType.length > 0) {
+            return 1;
+        } else if (aStationType.length > 0 && bStationType.length === 0) {
+            return -1;
+        }
+
+        const stationType = aStationType.localeCompare(bStationType);
+        return stationType !== 0 ? stationType : aStationName.localeCompare(bStationName);
+    });
+}
