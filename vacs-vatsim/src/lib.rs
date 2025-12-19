b1b3a697c2a6c2f4bad92528718a4c5df50702af
Nick Müller <nick.mueller@mailbox.org>
2025-12-19 10:11:50 +0100

    feat(vacs-vatsim): extend FacilityType parsing and formatting add VATSIM data feed facility type matching serialize and deserialize using suffix and enum matching

--- vacs-vatsim/src/lib.rs
+++ vacs-vatsim/src/lib.rs
@@ -1,6 +1,7 @@
 pub mod data_feed;
 pub mod slurper;
 
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
 use std::str::FromStr;
 use thiserror::Error;
 
@@ -31,7 +32,7 @@ pub struct ControllerInfo {
 
 /// Enum representing the different VATSIM facility types as parsed from their respective callsign suffixes
 /// (in accordance with the [VATSIM GCAP](https://vatsim.net/docs/policy/global-controller-administration-policy).
-#[derive(Debug, Clone, PartialEq, Eq, Hash, Default)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Default)]
 pub enum FacilityType {
     #[default]
     Unknown,
@@ -47,22 +48,46 @@ pub enum FacilityType {
     TrafficFlow,
 }
 
+impl FacilityType {
+    pub const fn as_str(&self) -> &str {
+        match self {
+            FacilityType::Ramp => "RMP",
+            FacilityType::Delivery => "DEL",
+            FacilityType::Ground => "GND",
+            FacilityType::Tower => "TWR",
+            FacilityType::Approach => "APP",
+            FacilityType::Departure => "DEP",
+            FacilityType::Enroute => "CTR",
+            FacilityType::FlightServiceStation => "FSS",
+            FacilityType::Radio => "RDO",
+            FacilityType::TrafficFlow => "FMP",
+            FacilityType::Unknown => "UNKNOWN",
+        }
+    }
+
+    pub fn from_vatsim_facility(facility: u8) -> Self {
+        FacilityType::try_from(facility).unwrap_or_default()
+    }
+}
+
 impl FromStr for FacilityType {
     type Err = Error;
     fn from_str(s: &str) -> Result<Self> {
         let s = s.to_ascii_uppercase();
         let facility_suffix = s.split('_').next_back().unwrap_or_default();
         match facility_suffix {
-            "RMP" => Ok(FacilityType::Ramp),
-            "DEL" => Ok(FacilityType::Delivery),
-            "GND" => Ok(FacilityType::Ground),
-            "TWR" => Ok(FacilityType::Tower),
-            "APP" => Ok(FacilityType::Approach),
-            "DEP" => Ok(FacilityType::Departure),
-            "CTR" => Ok(FacilityType::Enroute),
-            "FSS" => Ok(FacilityType::FlightServiceStation),
-            "RDO" => Ok(FacilityType::Radio),
-            "TMU" | "FMP" => Ok(FacilityType::TrafficFlow),
+            "RMP" | "RAMP" => Ok(FacilityType::Ramp),
+            "DEL" | "DELIVERY" => Ok(FacilityType::Delivery),
+            "GND" | "GROUND" => Ok(FacilityType::Ground),
+            "TWR" | "TOWER" => Ok(FacilityType::Tower),
+            "APP" | "APPROACH" => Ok(FacilityType::Approach),
+            "DEP" | "DEPARTURE" => Ok(FacilityType::Departure),
+            "CTR" | "CENTER" | "ENROUTE" => Ok(FacilityType::Enroute),
+            "FSS" | "FLIGHTSERVICESTATION" => Ok(FacilityType::FlightServiceStation),
+            "RDO" | "RADIO" => Ok(FacilityType::Radio),
+            "TMU" | "TRAFFICMANAGEMENTUNIT" | "FMP" | "FLOWMANAGEMENTPOSITION" | "TRAFFICFLOW" => {
+                Ok(FacilityType::TrafficFlow)
+            }
             other => Err(Error::UnknownFacilityType(other.to_string())),
         }
     }
@@ -77,5 +102,39 @@ impl From<&str> for FacilityType {
 impl From<String> for FacilityType {
     fn from(value: String) -> Self {
         value.as_str().parse().unwrap_or_default()
+    }
+}
+
+impl TryFrom<u8> for FacilityType {
+    type Error = Error;
+    fn try_from(value: u8) -> Result<Self> {
+        match value {
+            1 => Ok(FacilityType::FlightServiceStation),
+            2 => Ok(FacilityType::Delivery),
+            3 => Ok(FacilityType::Ground),
+            4 => Ok(FacilityType::Tower),
+            5 => Ok(FacilityType::Approach),
+            6 => Ok(FacilityType::Enroute),
+            other => Err(Error::UnknownFacilityType(other.to_string())),
+        }
+    }
+}
+
+impl Serialize for FacilityType {
+    fn serialize<S>(&self, serializer: S) -> std::result::Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        serializer.serialize_str(self.as_str())
+    }
+}
+
+impl<'de> Deserialize<'de> for FacilityType {
+    fn deserialize<D>(deserializer: D) -> std::result::Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let s = String::deserialize(deserializer)?;
+        FacilityType::from_str(&s).map_err(serde::de::Error::custom)
     }
 }
