d9d79c5ae8eec6390f55dff21cfbbb15034941d2
Luis Kreihsl <67544656+IamLuisAUT@users.noreply.github.com>
2026-02-06 16:31:55 +0100

    feat: implement direct access subpages (#491)
    * feat: implement direct access subpages
    
    * fix(vacs-client): reset profile store when receiving a new test profile
    
    * feat(vacs-client): allow multiple layers of subpages

--- vacs-client/frontend/src/components/DirectAccessPage.tsx
+++ vacs-client/frontend/src/components/DirectAccessPage.tsx
@@ -1,6 +1,11 @@
 import {DirectAccessPage as DirectAccessPageModel} from "../types/profile.ts";
 import {CSSProperties} from "preact";
 import DirectAccessStationKey from "./ui/DirectAccessStationKey.tsx";
+import {clsx} from "clsx";
+import ButtonLabel from "./ui/ButtonLabel.tsx";
+import Button from "./ui/Button.tsx";
+import {useProfileStore} from "../stores/profile-store.ts";
+import {useCallState} from "../hooks/call-state-hook.ts";
 
 type DirectAccessPageProps = {
     data: DirectAccessPageModel;
@@ -15,17 +20,55 @@ function DirectAccessPage({data}: DirectAccessPageProps) {
     return (
         <div className="w-full h-full overflow-auto">
             <div className="w-min min-h-full py-3 px-2 grid gap-2" style={style}>
-                {data.keys.map((key, index) => (
-                    <DirectAccessStationKey
-                        key={index}
-                        data={key}
-                        className={
-                            data.rows !== undefined && data.rows > 6 ? "leading-4!" : "leading-4.5!"
-                        }
-                    />
-                ))}
+                {data.keys.map((key, index) =>
+                    key.page !== undefined ? (
+                        <DirectAccessSubpageKey
+                            key={index}
+                            label={key.label}
+                            page={key.page}
+                            parent={data}
+                        />
+                    ) : (
+                        <DirectAccessStationKey
+                            key={index}
+                            data={key}
+                            className={
+                                data.rows !== undefined && data.rows > 6
+                                    ? "leading-4!"
+                                    : "leading-4.5!"
+                            }
+                        />
+                    ),
+                )}
             </div>
         </div>
+    );
+}
+
+type DirectAccessSubpageKeyProps = {
+    label: string[];
+    page: DirectAccessPageModel;
+    parent: DirectAccessPageModel;
+    className?: string;
+};
+
+function DirectAccessSubpageKey(props: DirectAccessSubpageKeyProps) {
+    const {beingCalled, isRejected, color} = useCallState(props.page);
+    const setSubpage = useProfileStore(state => state.setSubpage);
+
+    return (
+        <Button
+            color={color}
+            highlight={beingCalled || isRejected ? "green" : undefined}
+            className={clsx(
+                props.className,
+                "w-25 h-full rounded",
+                color === "gray" ? "p-1.5" : "p-[calc(0.375rem+1px)]",
+            )}
+            onClick={() => setSubpage(props.page, props.parent)}
+        >
+            <ButtonLabel label={props.label} />
+        </Button>
     );
 }
 
