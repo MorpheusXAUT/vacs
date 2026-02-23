88c6beb8e7c1975e88c093d9679c4454ac11c613
Luis Kreihsl <luis.kreihsl@gmx.at>
2026-02-23 14:23:00 +0100

    feat(vacs-client): log webview error and unhandledrejection in backend

--- vacs-client/frontend/src/main.tsx
+++ vacs-client/frontend/src/main.tsx
@@ -1,5 +1,21 @@
 import {render} from "preact";
 import App from "./App";
 import "./styles/main.css";
+import {error} from "@tauri-apps/plugin-log";
+import {safeSerialize} from "./error.ts";
+
+window.addEventListener("error", ev => {
+    void error(
+        `Webview error: ${JSON.stringify({
+            filename: ev.filename,
+            lineno: ev.lineno,
+            colno: ev.colno,
+            error: safeSerialize(ev.error),
+        })}`,
+    );
+});
+window.addEventListener("unhandledrejection", ev => {
+    void error(`Unhandled webview rejection: ${JSON.stringify(safeSerialize(ev.reason))}`);
+});
 
 render(<App />, document.getElementById("root")!);
