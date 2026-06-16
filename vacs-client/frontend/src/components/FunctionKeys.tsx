dbfa0d91b2f16062c8d4385946d6f332ecc6d9d7
Luis Kreihsl <luis.kreihsl@gmx.at>
2026-06-16 14:05:59 +0200

    refactor(vacs-client): remove fast cpl button from function keys

--- vacs-client/frontend/src/components/FunctionKeys.tsx
+++ vacs-client/frontend/src/components/FunctionKeys.tsx
@@ -4,7 +4,6 @@ import {useCallStore} from "../stores/call-store.ts";
 import {useSettingsStore} from "../stores/settings-store.ts";
 import Button from "./ui/Button.tsx";
 import LinkButton from "./ui/LinkButton.tsx";
-import FastCplButton from "./ui/FastCplButton.tsx";
 
 function FunctionKeys() {
     const prio = useCallStore(state => state.prio);
@@ -23,7 +22,9 @@ function FunctionKeys() {
             <Button color="cyan" className="text-slate-400" disabled={true}>
                 HOLD
             </Button>
-            <FastCplButton />
+            <Button color="cyan" className="text-slate-400" disabled={true}>
+                PICKUP
+            </Button>
             <Button color="cyan" className="text-slate-400" disabled={true}>
                 <p>
                     SUITE
