52fe077e9e574539d7ab7ee7b93cf1c147879b11
Luis Kreihsl <luis.kreihsl@gmx.at>
2026-07-16 14:30:45 +0200

    refactor(vacs-client): split transmit mode settings into several files

---
+++ vacs-client/frontend/src/components/ui/RadioPrioBadge.tsx
@@ -0,0 +1,9 @@
+function RadioPrioBadge() {
+    return (
+        <span className="bg-[#92e1fe] border-2 border-t-cyan-100 border-l-cyan-100 border-r-cyan-950 border-b-cyan-950 rounded px-1 text-xs text-black font-semibold">
+            RADIO PRIO
+        </span>
+    );
+}
+
+export default RadioPrioBadge;
